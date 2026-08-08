bcd1152cd13c0c0f80f4a61e5b4891030176d58e
zaidmeo09-dhangal <zaidmeo09@gmail.com>
2026-08-09 03:10:35 +0500

    Update analytics.js

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,1 +1,11 @@
+window.dataLayer = window.dataLayer || [];
 
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-QFNEKR7QE0', {
+    anonymize_ip: true
+});
