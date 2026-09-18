3007d0db3fc9c92f65ab02e1248169cd02b512bf
zaidmeo09-dhangal <zaidmeo09@gmail.com>
2026-09-19 02:58:52 +0500

    Add files via upload

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -9,3 +9,18 @@ gtag('js', new Date());
 gtag('config', 'G-QFNEKR7QE0', {
     anonymize_ip: true
 });
+
+function loadGoogleAnalytics() {
+    if (window.gaScriptLoaded) return;
+    window.gaScriptLoaded = true;
+
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-QFNEKR7QE0';
+    document.head.appendChild(script);
+}
+
+window.addEventListener('scroll', loadGoogleAnalytics, { once: true, passive: true });
+window.addEventListener('mousemove', loadGoogleAnalytics, { once: true });
+window.addEventListener('touchstart', loadGoogleAnalytics, { once: true, passive: true });
+setTimeout(loadGoogleAnalytics, 3500);
